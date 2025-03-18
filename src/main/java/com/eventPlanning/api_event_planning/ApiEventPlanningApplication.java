393ca52d999d31986cddb58797fa570599e35217
WesleyZanon <wesleyzanon17@gmail.com>
2025-03-18 19:40:19 -0300

    Initial commit

---
+++ src/main/java/com/eventPlanning/api_event_planning/ApiEventPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.eventPlanning.api_event_planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventPlanningApplication.class, args);
+	}
+
+}
